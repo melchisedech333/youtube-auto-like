a085a6a5b6438c9e01a4274859071cb3a5d069f3
melchisedech333 <melchisedech333@gmail.com>
2022-10-31 02:26:05 -0300

    Updates

--- background.js
+++ background.js
@@ -14,22 +14,12 @@
 		if (intervalRunning == false) {
 			intervalRunning = true;
 
-			var els = document.querySelectorAll('#button');
+			var els = document.querySelectorAll('yt-animated-icon');
 			for (var a=0; a<els.length; a++) {
 				if (els[a]) {
-					var item = '';
-					
-					if (els[a].innerHTML) {
-						item = els[a].innerHTML;
-						item = item.toString();
-					}
-					
-					if (item.indexOf('like') != -1 && 
-						item.indexOf('Dislike') == -1 &&
-						item.indexOf('aria-pressed="false"') != -1)
-					{
-						els[a].click();
-					}
+					els[a].click();
+					clearInterval(interval);
+					break;
 				}
 			}
 
